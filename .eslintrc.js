fef3e83e9cfd5e71c81f209be1e1bde3bc9b833c
doc <zengdongchao@gmail.com>
2021-10-13 15:31:10 +0800

    add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   parser: "babel-eslint",
   rules: {
+    "semi": ["error", "never"],
+    "comma-dangle": ["error", "never"],
     "arrow-parens": ["error", "as-needed"],
     "no-use-before-define": ["error", { "functions": false }],
     "no-underscore-dangle": ["error", { "allowAfterThis": true }],
